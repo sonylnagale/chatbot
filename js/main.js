0b6d42a06877ecde1cb809636a5750ca6fb141d3
Sonyl Nagale <sonyl.nagale@nbcuni.com>
2017-12-03 17:55:26 -0500

    Initial commit x2

--- js/main.js
+++ js/main.js
@@ -3,14 +3,4 @@ document.getElementById('chatForm').addEventListener('submit', function(e) {
   var message = document.getElementById('chatInput').value;
 
   document.getElementById('dave').innerHTML += message + "<br/>";
-
-  respondToMessage(message);
 });
-
-function respondToMessage(chat) {
-  if (chat == "Open the pod bay doors") {
-    document.getElementById('hal').innerHTML += "I'm sorry Dave, I can't do that. <br/>";
-  } else {
-    document.getElementById('hal').innerHTML += "I'm sorry Dave, I didn't understand you. <br/>";
-  }
-}
